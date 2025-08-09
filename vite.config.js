fd86f53f498cd095ef6548c9517671259251f4ac
Mtutuzeli Ngobo <mtutuzeli.ngobo@gmail.com>
2025-08-09 08:35:12 +0200

    commented out server

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/table-booking/",
-  server: {
-    open: true,
-    port: 3001,
-  },
+  // server: {
+  //   open: true,
+  //   port: 3001,
+  // },
 });
