e2f6d7c25a9b9ce37470a1fea6757c1fb2abd114
Mtutuzeli Ngobo <mtutuzeli.ngobo@gmail.com>
2025-08-13 12:32:10 +0200

    removed inline css

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,52 +1,42 @@
 import React from 'react'
+import foodImage from '../assets/images/restauranfood.jpg';
+import './Home.css'
+
+
 
 function Home() {
   return (
-    <div>
-      <div
-        id='homepage'
-        className="homepage"
-        style={{
-          height: "520px",
-          width:"100%",
-          // margin: "auto",
-        }}
-      >
-        <div className="homepage-cards">
-          <div
-            className="card"
-            style={{marginTop: "40px"}}
-          >
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>
-              <span
-                style={{display: "block"}}
-              >
-                We are a family-owned
-              </span>
-                Mediterranean restaurant,
-              <span
-                style={{display: "block"}}
-              >
-                focused on traditional
-              </span>
-                recipes served with a modern
-              <span
-                style={{display: "block"}}
-              >
-                twist.
-              </span>
-            </p>
-            <button>Reserve a table</button>
-          </div>
-          <div className="card restaurant-image">
-              <img
-                id='img-restaurantFood'
-                src="./images/restauranfood.jpg"
-                style={{width: "375px", height: "464px", borderRadius: "20px", marginTop: "50px"}}
-              />
-          </div>
+    <div id='homepage' className="homepage">
+      <div className="homepage-cards">
+        <div
+          className="card"
+          style={{marginTop: "40px"}}
+        >
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            <span
+              style={{display: "block"}}
+            >
+                 We are a family-owned
+            </span>
+              Mediterranean restaurant,
+            <span
+              style={{display: "block"}}
+            >
+              focused on traditional
+            </span>
+              recipes served with a modern
+            <span
+              style={{display: "block"}}
+            >
+              twist.
+            </span>
+          </p>
+          <button>Reserve a table</button>
+        </div>
+        <div className="card restaurant-image">
+            <img id='img-restaurantFood' src={foodImage} alt="Restaurant Food" />
         </div>
       </div>
     </div>
