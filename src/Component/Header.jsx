ec1e28010f89d8f0efe836cbb90bdeda3c67960a
Mtutuzeli Ngobo <mtutuzeli.ngobo@gmail.com>
2025-08-13 12:24:43 +0200

    moved images from public to asses folder

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Header.css'
+import logoImage from '../assets/images/Logo.png';
+import { Link } from 'react-router-dom';
+
+
 
 function Header() {
   return (
     <div className='header'>
-      <a href='#' target='blanket'><img src="./images/Logo.png" alt="logo"/></a>
+      <Link to="/">
+        <img
+          src={logoImage}
+          alt="logo"
+          style={{ width: '199px', height: '50px' }}
+        />
+      </Link>
     </div>
   )
 }
