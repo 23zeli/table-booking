71434b43a8a0a1db63d45cff55338c976d65f991
Mtutuzeli Ngobo <mtutuzeli.ngobo@gmail.com>
2025-08-22 10:21:56 +0200

    added container div

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -2,41 +2,41 @@ import React from 'react'
 import foodImage from '../assets/images/restauranfood.jpg';
 import './Home.css'
 
-
-
 function Home() {
   return (
-    <div id='homepage' className="homepage">
-      <div className="homepage-cards">
-        <div
-          className="card"
-          style={{marginTop: "40px"}}
-        >
-          <h1>Little Lemon</h1>
-          <h2>Chicago</h2>
-          <p>
-            <span
-              style={{display: "block"}}
-            >
-                 We are a family-owned
-            </span>
-              Mediterranean restaurant,
-            <span
-              style={{display: "block"}}
-            >
-              focused on traditional
-            </span>
-              recipes served with a modern
-            <span
-              style={{display: "block"}}
-            >
-              twist.
-            </span>
-          </p>
-          <button>Reserve a table</button>
-        </div>
-        <div className="card restaurant-image">
-            <img id='img-restaurantFood' src={foodImage} alt="Restaurant Food" />
+    <div className="homepage-container">
+      <div id='homepage' className="homepage">
+        <div className="homepage-cards">
+          <div
+            className="card"
+            style={{marginTop: "40px"}}
+          >
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>
+              <span
+                style={{display: "block"}}
+              >
+                   We are a family-owned
+              </span>
+                Mediterranean restaurant,
+              <span
+                style={{display: "block"}}
+              >
+                focused on traditional
+              </span>
+                recipes served with a modern
+              <span
+                style={{display: "block"}}
+              >
+                twist.
+              </span>
+            </p>
+            <button>Reserve a table</button>
+          </div>
+          <div className="card restaurant-image">
+              <img id='img-restaurantFood' src={foodImage} alt="Restaurant Food" />
+          </div>
         </div>
       </div>
     </div>
