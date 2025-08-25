9cdb8d8ccdfa3cdfb7dc53e6a4b5fb87dd99e04a
Mtutuzeli Ngobo <mtutuzeli.ngobo@gmail.com>
2025-08-25 09:37:04 +0200

    imported specials

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import foodImage from '../assets/images/restauranfood.jpg';
 import './Home.css'
+import Specials from './Specials';
 
 function Home() {
   const navigate = useNavigate();
@@ -11,39 +12,42 @@ function Home() {
   };
 
   return (
-    <div id='homepage' className="homepage">
-      <div className="homepage-cards">
-        <div
-          className="card"
-          style={{marginTop: "40px"}}
-        >
-          <h1>Little Lemon</h1>
-          <h2>Chicago</h2>
-          <p>
-            <span
-              style={{display: "block"}}
-            >
-                 We are a family-owned
-            </span>
-              Mediterranean restaurant,
-            <span
-              style={{display: "block"}}
-            >
-              focused on traditional
-            </span>
-              recipes served with a modern
-            <span
-              style={{display: "block"}}
-            >
-              twist.
-            </span>
-          </p>
-          <button onClick={handleReserveTable}>Reserve a table</button>
-        </div>
-        <div className="card restaurant-image">
-            <img id='img-restaurantFood' src={foodImage} alt="Restaurant Food" />
+    <div>
+      <div id='homepage' className="homepage">
+        <div className="homepage-cards">
+          <div
+            className="card"
+            style={{marginTop: "40px"}}
+          >
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>
+              <span
+                style={{display: "block"}}
+              >
+                   We are a family-owned
+              </span>
+                Mediterranean restaurant,
+              <span
+                style={{display: "block"}}
+              >
+               focused on traditional
+              </span>
+                recipes served with a modern
+              <span
+                style={{display: "block"}}
+              >
+                twist.
+              </span>
+            </p>
+            <button onClick={handleReserveTable}>Reserve a table</button>
+          </div>
+          <div className="card restaurant-image">
+              <img id='img-restaurantFood' src={foodImage} alt="Restaurant Food" />
+          </div>
         </div>
       </div>
+      <Specials />
     </div>
   )
 }
